fefacd4c839b6d818a29e20f9f68a7c18153be33
Ernesto Elias Aquino Cifuentes <ernesto@macbook-pro-de-ernesto.home>
2022-06-06 15:10:24 +0200

    Working in ingredients interface

---
+++ Reciplease/Reciplease/Controller/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  Reciplease
+//
+//  Created by Ernesto Elias Aquino Cifuentes on 06/06/2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var ingredientTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
