424264b89b4d58eccb3d0d3877c09db008d400a2
Ernesto Elias Aquino Cifuentes <ernesto@macbook-pro-de-ernesto.home>
2022-06-12 14:33:39 +0200

    Create func clear list ingredients

--- Reciplease/Reciplease/Controller/SearchViewController.swift
+++ Reciplease/Reciplease/Controller/SearchViewController.swift
@@ -15,26 +15,29 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var clearButton: UIButton!
     @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    let recipeService = RecipeService()
     
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
         ingrentTextField.resignFirstResponder()
     }
 
     @IBAction func addButtonTapped() {
+        recipeService.addIngredients(ingrentTextField.text)
         
     }
 
     @IBAction func clearButtonTapped() {
-        listIngredientTextView.text = ""
+        recipeService.clearListIngredient()
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        recipeService.viewDelegate = self
         addButton.round()
         clearButton.round()
         searchButton.round()
-        // Do any additional setup after loading the view.
     }
 
 }
@@ -51,5 +54,21 @@ extension SearchViewController: SearchDelegate {
         let action = UIAlertAction(title: "OK", style: .cancel)
         alert.addAction(action)
         present(alert, animated: true)
+    }
+
+    /**
+     This function adds an new element to list ingredient text view.
+     
+     - parameter value: String to be added to text view.
+     */
+    func refreshListIngredientWith(_ value: String) {
+        listIngredientTextView.text.append(value)
+    }
+
+    /**
+     This function clears the list of ingredients.
+     */
+    func clearListIngredient() {
+        listIngredientTextView.text = ""
     }
 }
