1b61484c6b036c72e1aabd147d2ae4b5f552d297
Ernesto Elias Aquino Cifuentes <ernesto@macbook-pro-de-ernesto.home>
2022-06-13 14:13:24 +0200

    Add func add ingredients

--- Reciplease/Reciplease/Model/RecipeService.swift
+++ Reciplease/Reciplease/Model/RecipeService.swift
@@ -13,11 +13,43 @@ class RecipeService {
 
 // MARK: - Funtions
     func addIngredients(_ ingredients: String?) {
-        guard let ingredients = ingredients,
-              !ingredients.isEmpty  else {
-            warningMessage("Please enter a valid ingredient.")
+        guard let ingredients = ingredients else {return}
+        let ingredientWithoutSpaces =  trimmingAllSpaces(ingredients)
+        if ingredientWithoutSpaces.isEmpty {
             return
         }
-        refreshListIngredientWith("\n- \(ingredients.capitalized)")
+        if ingredientWithoutSpaces.contains(",") {
+            let tabIngredients = splitIngredients(ingredientWithoutSpaces)
+            for ingredient in tabIngredients {
+                refreshListIngredientWith("- " + ingredient.capitalized + "\n")
+            }
+            clearIngredientTextField()
+        } else {
+            refreshListIngredientWith("- " + ingredientWithoutSpaces.capitalized + "\n")
+            clearIngredientTextField()
+        }
+    }
+
+    /**
+     This function trims a string using the comma " , " as a reference.
+     
+     - parameter ingredients: The string to be trimmed
+     
+     - returns: Returns a string table with the trimmed elements.
+     */
+    private func splitIngredients(_ ingredients: String) -> [String] {
+        let ingredientsWithoutSpaces = trimmingAllSpaces(ingredients)
+        return ingredientsWithoutSpaces.split(separator: ",").map{"\($0)"}
+    }
+
+    /**
+     This function removes whitespace from a string.
+     
+     - parameter string: String with spaces to be removed.
+     
+     - returns: String without spaces or line breaks.
+     */
+    private func trimmingAllSpaces(_ string: String) -> String {
+        return string.components(separatedBy: .whitespacesAndNewlines).joined()
     }
 }
