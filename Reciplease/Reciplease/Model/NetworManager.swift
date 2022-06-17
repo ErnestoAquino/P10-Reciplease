c4dba027d65e09889e9a142b061238cd029589fa
Ernesto Elias Aquino Cifuentes <ernesto@macbook-pro-de-ernesto.home>
2022-06-17 18:46:15 +0200

    Test func get image

--- Reciplease/Reciplease/Model/NetworManager.swift
+++ Reciplease/Reciplease/Model/NetworManager.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import Alamofire
+
+class NetworkManager <T: Decodable>{
+    
+    public func getInformation(request: URLRequest?, completionHandler: @escaping (T?, Error?) -> Void) {
+        guard let request = request else {
+            completionHandler(nil, nil)
+            return
+        }
+
+        AF.request(request).responseDecodable(of: RecipeResponse.self) { response in
+            guard response.error == nil else {
+                completionHandler(nil, response.error)
+                return
+            }
+            guard let data = response.data,
+                  response.response?.statusCode == 200 else {
+                completionHandler(nil, nil)
+                return
+            }
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .useDefaultKeys
+            decoder.dateDecodingStrategy = .secondsSince1970
+            guard let recipeResponse =  try? decoder.decode(T.self, from: data) else {
+                completionHandler(nil, nil)
+                return
+            }
+            completionHandler(recipeResponse, nil)
+        }
+    }
+
+// MARK: -Funciones de test.
+
+}
