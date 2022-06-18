00140fbbcb68b8184182f892a2fc970885ff17f8
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-06-18 15:43:49 +0200

    Addin structure decodable

--- Reciplease/Reciplease/Structures/RecipeResponse.swift
+++ Reciplease/Reciplease/Structures/RecipeResponse.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+
+struct RecipeResponse: Decodable {
+    let from: Int?
+    let towards: Int?
+    let count: Int?
+    let links: Links?
+    let hits: [Hit]?
+
+    private enum CodingKeys: String, CodingKey {
+        case from
+        case towards = "to"
+        case count
+        case links = "_links"
+        case hits
+    }
+}
+
+struct Hit: Decodable {
+    var recipe: Recipe?
+    var links: Links?
+
+    private enum CodingKeys: String, CodingKey {
+        case recipe
+        case links = "_links"
+    }
+}
+
+struct Recipe: Decodable {
+    var label: String?
+    var image: String?
+    var source: String?
+    var url: String?
+    var yield: Float?
+    var ingredientLines: [String]?
+    var ingredients: [Ingredient]?
+    var totalTime: Float?
+    var imageData: Data?
+}
+
+struct Ingredient: Decodable {
+    var text: String?
+    var quantity: Double?
+    var measure: String?
+//    var food: String?
+    var weight: Double?
+//    var fodID: String?
+}
+
+struct Links: Decodable {
+    let same: Link?
+    let next: Link?
+
+    private enum CodingKeys: String, CodingKey {
+        case same = "self"
+        case next
+    }
+}
+
+struct Link: Decodable {
+    let href: String?
+    let title: String?
+}
