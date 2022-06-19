d4cc5965090cfea6a2b08fbc8255fbb520ee03f6
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-06-19 13:21:00 +0200

    adjustment in the border color.

--- Reciplease/Reciplease/Extensions/UIImageViewExtension.swift
+++ Reciplease/Reciplease/Extensions/UIImageViewExtension.swift
@@ -10,11 +10,15 @@ import UIKit
 
 extension UIImageView {
     
+    /**
+     This function rounds a UIIMageView and adds a green border around it.
+     */
     func makeRounded() {
+        let greenRecipe =  UIColor(red: 0.3529, green: 0.5725, blue: 0.3843, alpha: 1.0)
         
-        layer.borderWidth = 1
+        layer.borderWidth = 2
         layer.masksToBounds = false
-        layer.borderColor = UIColor.black.cgColor
+        layer.borderColor = greenRecipe.cgColor
         layer.cornerRadius = self.frame.height / 2
         clipsToBounds = true
     }
