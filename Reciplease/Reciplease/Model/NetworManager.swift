049db3aadd602ac3563701f81c69e41ef88e0129
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-06-22 15:21:14 +0200

    Creating class test_nerworkmamager

--- Reciplease/Reciplease/Model/NetworManager.swift
+++ Reciplease/Reciplease/Model/NetworManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Alamofire
+import AlamofireImage
 
 class NetworkManager <T: Decodable>{
     
@@ -36,6 +37,99 @@ class NetworkManager <T: Decodable>{
             completionHandler(recipeResponse, nil)
         }
     }
-
-// MARK: -Funciones de test.
 }
+
+
+class test_networkManager {
+
+    public func getInformation<T: Decodable>(request: URLRequest?, completionHandler: @escaping (T?, Error?)-> Void ) {
+        guard let request = request else {
+            completionHandler(nil, nil)
+            return
+        }
+
+        AF.request(request).responseDecodable(of: RecipeResponse.self) { response in
+            guard response.error == nil else {
+                completionHandler(nil, response.error)
+                return
+            }
+            guard let data = response.data,
+                  response.response?.statusCode == 200 else {
+                completionHandler(nil, nil)
+                return
+            }
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .useDefaultKeys
+            decoder.dateDecodingStrategy = .secondsSince1970
+            guard let recipeResponse =  try? decoder.decode(T.self, from: data) else {
+                completionHandler(nil, nil)
+                return
+            }
+            completionHandler(recipeResponse, nil)
+        }
+    }
+
+    public func getImage(url: String?, completionHandler: @escaping (Image?) -> Void) {
+        guard let urlImage = url else {
+            completionHandler(nil)
+            return
+        }
+        AF.request(urlImage).responseImage { response in
+            guard response.error != nil else {
+                completionHandler(nil)
+                return
+            }
+            completionHandler(response.value)
+        }
+    }
+
+    public func getImageTwo(url: String?) -> Data? {
+        var imageToReturn: Data?
+        guard let imageUrl = url else {
+            return nil
+        }
+        AF.request(imageUrl).responseImage { response in
+                guard response.error != nil,
+                      let data = response.data else {return}
+                imageToReturn = data
+        }
+
+        return imageToReturn
+    }
+
+    public func getImageTres(url: String?, completionHanler: @escaping (Data?, Bool) -> Void)  {
+        
+        guard let urlImage = url else {
+            completionHanler(nil, false)
+            return
+        }
+
+        AF.request(urlImage).responseImage { response in
+            DispatchQueue.main.async {
+                guard response.error != nil,
+                      let data = response.data else {
+                    completionHanler(nil, false)
+                    return
+                }
+
+                completionHanler(data, true)
+            }
+        }
+    }
+
+}
+
+
+//  if let imageUrl = recipe.image {
+//AF.request(imageUrl).responseImage { response in
+//    if case .success(let image) = response.result {
+//        print("image downloaded: \(image)")
+//        DispatchQueue.main.async {
+//            cell.imageRecipe.image = image
+//            cell.imageRecipe.isHidden = false
+//            cell.imageRecipe.contentMode = .scaleToFill
+//            cell.imageRecipe.makeRounded()
+//        }
+//    }
+//}
+//}
