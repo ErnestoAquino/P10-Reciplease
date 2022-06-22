61e9abc79be455abe5eb3099f2b7667beb47ef9a
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-06-22 15:06:27 +0200

    Adding variables for configurations vc

--- Reciplease/Reciplease/Controller/RecipeDetailViewController.swift
+++ Reciplease/Reciplease/Controller/RecipeDetailViewController.swift
@@ -9,10 +9,45 @@ import UIKit
 
 class RecipeDetailViewController: UIViewController {
 
+    @IBOutlet weak var imageRecipe: UIImageView!
+    @IBOutlet weak var titleRecipeLabel: UILabel!
+    @IBOutlet weak var preparationTimeLabel: UILabel!
+    @IBOutlet weak var portionsRecipeLabel: UILabel!
+    @IBOutlet weak var detailRecipeText: UITextView!
+    @IBOutlet weak var getDirectionsButton: UIButton!
+
+    let defaultImage = UIImage(named: "defaultImage")
+
+    var img: Data?
+    var titleRecipe: String?
+    var time: String?
+    var portions: String?
+    var details: [String]?
+    var urlSource: String?
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        titleRecipeLabel.text = titleRecipe
+        preparationTimeLabel.text = time
+        portionsRecipeLabel.text = portions
+        detailRecipeText.text = details?.joined(separator: "\n")
+        if let dataImage = img {
+            imageRecipe.image = UIImage(data: dataImage)
+        } else {
+            imageRecipe.image = defaultImage
+        }
+
+        getDirectionsButton.round()
+    }
+    
+    @IBAction func getDirectionButtonTaped() {
+        guard let urlSource = urlSource,
+              let url = URL(string: urlSource) else {
+                  return
+              }
+        UIApplication.shared.open(url)
     }
     
 
