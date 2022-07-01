1b1ef6b0610ff6066ea03e9e01f2c99c59fc7764
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-07-02 00:31:16 +0200

    Write the description of methods

--- Reciplease/Reciplease/Model/NetworManager.swift
+++ Reciplease/Reciplease/Model/NetworManager.swift
@@ -9,8 +9,20 @@ import Foundation
 import Alamofire
 import AlamofireImage
 
-class NetworkManager <T: Decodable>{
-    
+/**
+ * NetworkManager
+ *
+ * This class handles network requests using the Alamofire and AlamofireImage packets.
+ */
+public final class NetworkManager <T: Decodable>{
+
+    /**
+     This function retrieves the contents of a URL based on the specified URL request object, and calls a handler  completion.
+     
+     - parameter request:           A URL request object that provides the URL, request type, body data or body stream, and so on.
+     - parameter completionHandler: This completion handler takes the following parameters: T? : The data returned by the server like a structure decodable type. Error? :
+      An error object that indicates why the request failed, or nil if the request was successful.
+     */
     public func getInformation(request: URLRequest?, completionHandler: @escaping (T?, Error?) -> Void) {
         guard let request = request else {
             completionHandler(nil, nil)
@@ -38,21 +50,12 @@ class NetworkManager <T: Decodable>{
         }
     }
 
-//    public func getImage(url: String?, completionHandler: @escaping (Data?) -> Void) {
-//        guard let url = url else {
-//            completionHandler(nil)
-//            return
-//        }
-//        AF.request(url).responseImage { response in
-//                guard response.error == nil,
-//                      let data = response.data else {
-//                    completionHandler(nil)
-//                    return
-//                }
-//                completionHandler(data)
-//            }
-//    }
-
+    /**
+     This function retrieves an image from the url passed in parameter.
+     
+     - parameter url:               String url for request
+     - parameter completionHandler:  This completion handler takes the following parameters: Data? : The data returned by the server if the operation was successful or nil if there was an error.
+     */
     public func getImage(url: String?, completionHandler: @escaping (Data?) -> Void) {
         guard let url = url else {
             completionHandler(nil)
@@ -71,97 +74,3 @@ class NetworkManager <T: Decodable>{
     }
 }
 
-
-class test_networkManager {
-
-    public func getInformation<T: Decodable>(request: URLRequest?, completionHandler: @escaping (T?, Error?)-> Void ) {
-        guard let request = request else {
-            completionHandler(nil, nil)
-            return
-        }
-
-        AF.request(request).responseDecodable(of: RecipeResponse.self) { response in
-            guard response.error == nil else {
-                completionHandler(nil, response.error)
-                return
-            }
-            guard let data = response.data,
-                  response.response?.statusCode == 200 else {
-                completionHandler(nil, nil)
-                return
-            }
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .useDefaultKeys
-            decoder.dateDecodingStrategy = .secondsSince1970
-            guard let recipeResponse =  try? decoder.decode(T.self, from: data) else {
-                completionHandler(nil, nil)
-                return
-            }
-            completionHandler(recipeResponse, nil)
-        }
-    }
-
-    public func getImage(url: String?, completionHandler: @escaping (Image?) -> Void) {
-        guard let urlImage = url else {
-            completionHandler(nil)
-            return
-        }
-        AF.request(urlImage).responseImage { response in
-            guard response.error != nil else {
-                completionHandler(nil)
-                return
-            }
-            completionHandler(response.value)
-        }
-    }
-
-    public func getImageTwo(url: String?) -> Data? {
-        var imageToReturn: Data?
-        guard let imageUrl = url else {
-            return nil
-        }
-        AF.request(imageUrl).responseImage { response in
-                guard response.error != nil,
-                      let data = response.data else {return}
-                imageToReturn = data
-        }
-
-        return imageToReturn
-    }
-
-    public func getImageTres(url: String?, completionHanler: @escaping (Data?, Bool) -> Void)  {
-        
-        guard let urlImage = url else {
-            completionHanler(nil, false)
-            return
-        }
-
-        AF.request(urlImage).responseImage { response in
-            DispatchQueue.main.async {
-                guard response.error != nil,
-                      let data = response.data else {
-                    completionHanler(nil, false)
-                    return
-                }
-
-                completionHanler(data, true)
-            }
-        }
-    }
-
-}
-
-
-//  if let imageUrl = recipe.image {
-//AF.request(imageUrl).responseImage { response in
-//    if case .success(let image) = response.result {
-//        print("image downloaded: \(image)")
-//        DispatchQueue.main.async {
-//            cell.imageRecipe.image = image
-//            cell.imageRecipe.isHidden = false
-//            cell.imageRecipe.contentMode = .scaleToFill
-//            cell.imageRecipe.makeRounded()
-//        }
-//    }
-//}
-//}
