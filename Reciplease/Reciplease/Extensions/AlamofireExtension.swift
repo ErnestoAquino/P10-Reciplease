d7c935b8b8b384e21bbdea2419d81926085dfaa8
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-07-11 18:41:59 +0200

    Extending the functions of alamofire to be able to perform tests.

--- Reciplease/Reciplease/Extensions/AlamofireExtension.swift
+++ Reciplease/Reciplease/Extensions/AlamofireExtension.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import Alamofire
+import AlamofireImage
+import SwiftUI
+
+
+protocol SessionProtocol {
+    func withRequest(_ convertible: URLRequestConvertible) -> DataRequestProtocol
+}
+
+protocol DataRequestProtocol {
+    func withResponse(completionHandler: @escaping (AFDataResponse<Data?>) -> Void)
+    func withResponseImage(completionHandler: @escaping (AFDataResponse<UIImage>) -> Void)
+}
+
+extension Session: SessionProtocol {
+    func withRequest(_ convertible: URLRequestConvertible) -> DataRequestProtocol {
+         request(convertible) as DataRequestProtocol
+    }
+}
+
+extension DataRequest: DataRequestProtocol {
+    func withResponseImage(completionHandler: @escaping (AFDataResponse<UIImage>) -> Void) {
+        responseImage(completionHandler: completionHandler)
+    }
+    
+
+    func withResponse(completionHandler: @escaping (AFDataResponse<Data?>) -> Void) {
+        response(completionHandler: completionHandler)
+    }
+}
