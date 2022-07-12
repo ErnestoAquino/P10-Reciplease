a270ef92e8f593b8eae33f1ad45fab4752df621c
ErnestoAquino <88657406+ErnestoAquino@users.noreply.github.com>
2022-07-12 14:18:31 +0200

    new function in protocol

--- Reciplease/Reciplease/Extensions/AlamofireExtension.swift
+++ Reciplease/Reciplease/Extensions/AlamofireExtension.swift
@@ -8,26 +8,31 @@
 import Foundation
 import Alamofire
 import AlamofireImage
-import SwiftUI
+//import SwiftUI
 
 
 protocol SessionProtocol {
     func withRequest(_ convertible: URLRequestConvertible) -> DataRequestProtocol
+    func withURL(_ convertible: URLConvertible) -> DataRequestProtocol
 }
 
 protocol DataRequestProtocol {
     func withResponse(completionHandler: @escaping (AFDataResponse<Data?>) -> Void)
-    func withResponseImage(completionHandler: @escaping (AFDataResponse<UIImage>) -> Void)
+    func withResponseImage(completionHandler: @escaping (AFDataResponse<Image>) -> Void)
 }
 
 extension Session: SessionProtocol {
+    func withURL(_ convertible: URLConvertible) -> DataRequestProtocol {
+        request(convertible) as DataRequestProtocol
+    }
+    
     func withRequest(_ convertible: URLRequestConvertible) -> DataRequestProtocol {
          request(convertible) as DataRequestProtocol
     }
 }
 
 extension DataRequest: DataRequestProtocol {
-    func withResponseImage(completionHandler: @escaping (AFDataResponse<UIImage>) -> Void) {
+    func withResponseImage(completionHandler: @escaping (AFDataResponse<Image>) -> Void) {
         responseImage(completionHandler: completionHandler)
     }
     
